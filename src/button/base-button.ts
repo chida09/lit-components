c6d0af750c6494d593bbe18a67771da2c9853aae
chida09 <t.chida0909@gmail.com>
2022-07-14 23:24:11 +0900

    base-buttonの作成

--- src/button/base-button.ts
+++ src/button/base-button.ts
@@ -1,48 +1,61 @@
 import { html, css, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
 @customElement('base-button')
 export class BaseButton extends LitElement {
   static styles = css`
-    :host {
-      display: block;
-      padding: 16px;
+    button {
+        display: inline-block;
+        line-height: 1;
+        white-space: nowrap;
+        cursor: pointer;
+        background: #fff;
+        border: 1px solid #dcdfe6;
+        color: #606266;
+        -webkit-appearance: none;
+        text-align: center;
+        box-sizing: border-box;
+        outline: none;
+        margin: 0;
+        transition: .1s;
+        font-weight: 500;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        padding: 12px 20px;
+        font-size: 14px;
+        border-radius: 4px;
+    }
+    
+    button:hover {
+        color: #409eff;
+        border-color: #c6e2ff;
+        background-color: #ecf5ff;
+    }
+    
+    button:focus {
+        color: #409eff;
+        border-color: #c6e2ff;
+        background-color: #ecf5ff;
+    }
+
+    button:active {
+        color: #3a8ee6;
+        border-color: #3a8ee6;
+        outline: none;
     }
   `
 
-  // https://qiita.com/sugasaki/items/6e9c2b7d11c8824b3dc3#1-text-content
-  @property()
-  myProp: { type: String; attribute: 'my-prop' }
-      /**
-       * The number of times the button has been clicked.
-       */
-      | undefined
-
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({ type: Number })
-  count = 0
+  @property({ type: String })
+  name = ''
 
   render() {
     return html`
-      <button @click=${this._onClick} part="button">
-        <slot></slot>
+      <button part='button'>
+        ${this.name}
       </button>
     `
   }
-
-  private _onClick() {
-    console.log('myProp', this.myProp)
-    this.count++
-  }
-
 }
 
 declare global {
